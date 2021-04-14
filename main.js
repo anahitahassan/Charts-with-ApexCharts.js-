5f3fa39e070e1f97c991caa60e69e6c61ad46a6f
anahitahassan <anahitarhassan@gmail.com>
2021-04-14 16:16:16 -0700

    horizontal button

--- main.js
+++ main.js
@@ -9,10 +9,47 @@ const options = {
     }, 
     series: [{
         name: 'Population', 
-        data: [8550405, 3971883, 2720546, 2296224, 1563025, 1469845, 1394928, 1300092, 1026908]
+        data: [
+            8550405, 
+            3971883, 
+            2720546, 
+            2296224, 
+            1567442,
+            1563025, 
+            1469845, 
+            1394928, 
+            1300092, 
+            1026908
+        ]
     }], 
     xaxis: {
-        categories: ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Philadelphia', 'Pheonix', 'San Antonio', 'San Diego', 'Dallas', 'San Jose']
+        categories: [
+            'New York', 
+            'Los Angeles', 
+            'Chicago', 
+            'Houston', 
+            'Philadelphia', 
+            'Pheonix', 
+            'San Antonio', 
+            'San Diego', 
+            'Dallas', 
+            'San Jose'
+        ],
+    }, 
+    fill: {
+        colors: ['#f44336']
+    }, 
+    dataLabels: {
+        enabled: false
+    },
+    title: {
+        text: 'Largest US Cities By Population', 
+        align: 'center', 
+        margin: 20, 
+        offsetY: 20, 
+        style: {
+            fontSize: '25px'
+        }
     }
 };
 
@@ -21,3 +58,17 @@ const chart = new ApexCharts(document.querySelector('#chart'), options);
 
 // Render Chart
 chart.render(); 
+
+// Event Method Example
+document.querySelector('button').addEventListener('click', () => 
+    chart.updateOptions({
+        plotOptions: {
+            bar: {
+                horizontal: true
+            }
+        }
+    })
+);
+
+
+
